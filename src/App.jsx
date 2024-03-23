aa923e30bc8afac078d214f4ebf8a32f2978264e
Yurei7 <114104437+Yurei7@users.noreply.github.com>
2024-03-23 15:59:54 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
-import { useState } from 'react'
-import numbers from "./numbers.jsx"
+import { useState, useEffect } from 'react'
+import confetti from 'canvas-confetti';
+import { data, getRandomNum } from "./data.js"
 
 
 function App() {
+  const [numbers, setNumbers] = useState(data)
+  const [win, setWin]=useState(false)
 
+
+  useEffect(() => {
+    const selectedTiles = numbers.filter(tile => tile.selected)
+    const firstValue = selectedTiles[0]?.value
+    const allSameValue = selectedTiles.every(tile => tile.value === firstValue)
+    if (allSameValue && selectedTiles.length === numbers.length) {
+       setWin(prevWin=>!prevWin)
+      launchConfetti()
+      }
+      else {
+
+      }
+  }, [numbers])
+
+
+  function roll() {
+    setNumbers(prevNumbers => {
+      return prevNumbers.map(function (tile) {
+        return tile.selected ? { ...tile, value: tile.value } : { ...tile, value: getRandomNum() }
+      })
+    })
+  }
+
+
+  function holdDice(tileId) {
+    setNumbers(prevNumbers => {
+      return prevNumbers.map(function (tile) {
+        if (tile.id === tileId) {
+          return { ...tile, value: tile.value, selected: !tile.selected }
+        }
+        else {
+          return tile
+        }
+      })
+    })
+  }
+
+  function launchConfetti() {
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 }
+    });
+  }
+
+function resetGame(){
+  setWin(false)
+  setNumbers(prevNumbers=> {
+    return prevNumbers.map(tile=> {
+      return {...tile, selected:false, value:getRandomNum()}
+    })
+  })
+}
 
   return <main className="main-container">
     <div className="text-container">
@@ -11,18 +67,14 @@ function App() {
       <h3>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
     </div>
     <div className="numbers-container">
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
-      <div className='tile'>1</div>
+      {numbers.map(tile => {
+        return <div onClick={() => holdDice(tile.id)} className={tile.selected ? "tile tile-active" : "tile"}>{tile.value}</div>
+      })}
 
     </div>
+    {!win && <button onClick={roll} className="btn">Roll</button>}
+    {win && <button onClick={resetGame} className="btn">Reset Game</button>}
+
 
   </main>
 }
---
+++ src/data.js
@@ -0,0 +1,67 @@
+export function getRandomNum() {
+    return Math.floor((Math.random() * 9) + 1)
+
+}
+
+export const data = [
+    {
+        id: 1,
+        value: getRandomNum(),
+        selected: false
+    },
+    {
+        id: 2,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 3,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 4,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 5,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 6,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 7,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 8,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 9,
+        value: getRandomNum(),
+        selected: false
+
+    },
+    {
+        id: 10,
+        value: getRandomNum(),
+        selected: false
+
+    }
+
+]
